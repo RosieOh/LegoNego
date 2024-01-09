dc5ef122d9b54fcb31689d7c3640cef2c02f9f59
[RosieOh] <[20172207@gm.hannam.ac.kr]>
2024-01-09 21:58:51 +0900

    🏪build : LegoNego 플랫폼 build🏪

---
+++ src/main/java/com/legonego/LegonegoApplication.java
@@ -0,0 +1,13 @@
+package com.legonego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegonegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegonegoApplication.class, args);
+	}
+
+}
