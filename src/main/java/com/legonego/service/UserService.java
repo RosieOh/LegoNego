190a11e1dae90a7b8131448e2a37ce08b233130e
[RosieOh] <[20172207@gm.hannam.ac.kr]>
2024-01-09 23:10:40 +0900

    🏪feat : 회훤처리 비즈니스 레이어 생성🏪

--- src/main/java/com/legonego/service/UserService.java
+++ src/main/java/com/legonego/service/UserService.java
@@ -1,6 +1,7 @@
 package com.legonego.service;
 
 import com.legonego.domain.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
@@ -13,5 +14,13 @@ public interface UserService {
     public User getByEmail(String email);
     public User getByName(String name);
     public User findById(String email, String tel);
-    public User 
+    public User findByPw(String email, String tel, String name);
+    public int userJoin(User user);
+    public int userUpdate(User user);
+    public int updateLevel(String name, String lev);
+    public int userRemove(String name);
+    public PasswordEncoder passwordEncoder();
+    public User getUserById(Integer id);
+    public User getUserByName(String name);
+    public int updatePasswordFailChange(User user);
 }
