61f5d31b1aea5ac55fe1e25317077ecb240a7818
RosieOh <20172207@gm.hannam.ac.kr>
2024-01-20 03:27:12 +0900

    🏪feat : 회원처리 및 인가 작업을 위한 Service 작성 완 #1🏪

---
+++ src/main/java/com/legonego/service/UserService.java
@@ -0,0 +1,27 @@
+package com.legonego.service;
+
+import com.legonego.domain.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+
+public interface UserService {
+    public List<User> userList();
+    public User getUser(String name);
+    public int getWithdraw(Integer id);
+    public int getActivate(String name);
+    public int getDormant(String name);
+    public User getByEmail(String email);
+    public User getByName(String name);
+    public User findById(String email, String tel);
+    public User findByPw(String email, String tel, String name);
+    public int userJoin(User user);
+    public int updateUser(User user);
+    public int updateLevel(String name, String lev);
+    public int updateUser(String name);
+    public int removeUser(String name);
+    public PasswordEncoder passwordEncoder();
+    public User getUserById(Integer id);
+    public User getUserByName(String name);
+    public int updatePasswordNoChange(User user);
+}
